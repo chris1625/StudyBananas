0d6867c5f32d199858857bf312955ade10ed789e
Chris Harris <crh013@ucsd.edu>
2017-02-02 11:11:53 -0800

    Integrated AD with login activity
    UCSD AD auth now integrated with the login activity.
    Next steps include shared preferences to keep users
    logged in, as well as a first-use prompt to save names
    in the database.

--- app/src/main/java/com/bananabanditcrew/studybananas/LoginActivity.java
+++ app/src/main/java/com/bananabanditcrew/studybananas/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,6 +29,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,13 +48,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -292,29 +289,28 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        private final String mDomain = "ad.ucsd.edu";
+        private final String mUser;
 
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
+            mUser = mEmail.substring(0,mEmail.indexOf("@"));
+            Log.d("LDAP Auth", "Got " + mUser + " from email.");
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
+            Log.d("LDAP Auth", "Starting LDAP authentication");
+            LDAPConnection connection;
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                String user = mUser + "@" + mDomain;
+                connection = new LDAPConnection(mDomain, 389, user, mPassword);
+                Log.d("LDAP Auth", "Authentication successful");
+            } catch (LDAPException e) {
+                Log.e("LDAP Auth", "Authentication failed");
                 return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
             }
 
             // TODO: register the new account here.
--- app/src/main/java/com/bananabanditcrew/studybananas/MainActivity.java
+++ app/src/main/java/com/bananabanditcrew/studybananas/MainActivity.java
@@ -15,32 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connectToLDAP("username", "password", "ad.ucsd.edu");
-    }
-
-    /* Test for Android LDAP library */
-    public void connectToLDAP(String username, String password, String domain) {
-        final String threadUsername = username;
-        final String threadPassword = password;
-        final String threadDomain = domain;
-
-        // Create a new thread to authenticate the user. Necessary, as network operations could
-        // potentially hold up the app without async operations
-        new Thread() {
-            public void run() {
-                Log.d("LDAP Auth", "Starting LDAP method");
-                LDAPConnection connection;
-                try {
-                    String user = threadUsername + "@" + threadDomain;
-                    connection = new LDAPConnection(threadDomain, 389, user, threadPassword);
-                    Log.d("LDAP Auth", "Authentication successful");
-                } catch (LDAPException e) {
-                    Log.e("LDAP Auth", "Authentication failed");
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-        return;
     }
 
     /* Opens login activity */
