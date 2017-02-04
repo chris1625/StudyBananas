96d5d9cd2473fcfc8f66b49470b32a58f619fcb5
Chris Harris <crh013@ucsd.edu>
2017-02-03 20:23:48 -0800

    Added a dialog for full name
    Created a dialogfragment for use when users are logging
    in for the first time. It is currently being called from the
    main class and does nothing, but will soon be moved to
    the loginActivity. Just pushing to mark some progress.
    
    Names entered are logged with the tag "Accounts" if
    the "Let's Go!" button is pressed.

--- app/src/main/java/com/bananabanditcrew/studybananas/MainActivity.java
+++ app/src/main/java/com/bananabanditcrew/studybananas/MainActivity.java
@@ -1,10 +1,13 @@
 package com.bananabanditcrew.studybananas;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
     public void openLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    /* Opens dialogue for the user */
+    public void showCreationDialogue(View view) {
+        FragmentManager fm = getSupportFragmentManager();
+        final CreationDialogFragment creationDialogFragment = CreationDialogFragment.newInstance("Some Title");
+        creationDialogFragment.show(fm, "fragment_creation_dialogue");
+    }
+
+    /* Callback method for dialogue */
+    public void setName(String name) {
+        Log.d("Accounts", name);
     }
 }
