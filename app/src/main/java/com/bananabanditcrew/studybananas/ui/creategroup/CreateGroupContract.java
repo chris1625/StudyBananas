3bc4f29984be8d99b19766d6d05f415b96743f83
Shimmy96 <shimmyyyy@gmail.com>
2017-02-24 14:52:10 -0800

    Added more files to work together on

---
+++ app/src/main/java/com/bananabanditcrew/studybananas/ui/creategroup/CreateGroupContract.java
@@ -0,0 +1,23 @@
+package com.bananabanditcrew.studybananas.ui.creategroup;
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+import com.bananabanditcrew.studybananas.ui.BasePresenter;
+import com.bananabanditcrew.studybananas.ui.BaseView;
+
+/**
+ * Created by Ryan on 2/24/2017.
+ */
+
+public interface CreateGroupContract {
+    interface View extends BaseView<Presenter> {
+        Activity getActivity();
+    }
+
+    interface Presenter extends BasePresenter{
+        ArrayAdapter<String> getCoursesAdapter();
+
+
+    }
+}
---
+++ app/src/main/java/com/bananabanditcrew/studybananas/ui/creategroup/CreateGroupPresenter.java
@@ -0,0 +1,34 @@
+package com.bananabanditcrew.studybananas.ui.creategroup;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import com.bananabanditcrew.studybananas.data.database.DatabaseCallback;
+import com.bananabanditcrew.studybananas.data.database.DatabaseHandler;
+
+/**
+ * Created by Ryan on 2/24/2017.
+ */
+
+public class CreateGroupPresenter implements DatabaseCallback, CreateGroupContract.Presenter {
+    private final CreateGroupContract.View mCreateGroupView;
+    private ArrayAdapter<String> mCourseList;
+    private DatabaseHandler mDatabase;
+
+    public CreateGroupPresenter(@NonNull CreateGroupContract.View mCreateGroupView,
+                                ArrayAdapter<String> courseList) {
+        this.mCreateGroupView = mCreateGroupView;
+        this.mCourseList = courseList;
+        this.mDatabase = new DatabaseHandler()
+    }
+
+    @Override
+    public void start() {
+        //// TODO: 2/24/2017
+    }
+
+    public Activity getActivity() {
+        return mCreateGroupView.getActivity();
+    }
+}
